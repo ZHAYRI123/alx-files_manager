29e1602fac8f08aeb55b56cd845f235f09884aa8
unknown <merykozan@gmail.com>
2024-12-11 23:54:07 +0100

    fixcode

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,4 @@
 import { MongoClient } from 'mongodb';
-
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
@@ -9,11 +8,12 @@ class DBClient {
     const uri = `mongodb://${host}:${port}`;
 
     this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    this.db = null;
 
     this.client.connect()
       .then(() => {
         this.db = this.client.db(database);
-        console.log('Connected to MongoDB');
+        console.log(`Connected to MongoDB at ${uri}`);
       })
       .catch((error) => {
         console.error(`MongoDB connection error: ${error.message}`);
@@ -21,19 +21,32 @@ class DBClient {
   }
 
   isAlive() {
-    return this.client.isConnected();
+    return this.client.topology && this.client.topology.isConnected();
   }
 
   async nbUsers() {
+    if (!this.db) return 0;
     const usersCollection = this.db.collection('users');
     return usersCollection.countDocuments();
   }
 
   async nbFiles() {
+    if (!this.db) return 0; 
     const filesCollection = this.db.collection('files');
     return filesCollection.countDocuments();
+  }
+
+  async close() {
+    await this.client.close();
   }
 }
 
 const dbClient = new DBClient();
+
+process.on('SIGINT', async () => {
+  console.log('Closing MongoDB connection');
+  await dbClient.close();
+  process.exit(0);
+});
+
 export default dbClient;
